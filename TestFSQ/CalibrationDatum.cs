a2ecfe8a02cab4d5bb7c2e1dd0eb91f1dc0a8f67
Xu Li <me@xuli.us>
2020-09-07 19:56:44 -0700

    NumberCellRenderer

--- TestFSQ/CalibrationDatum.cs
+++ TestFSQ/CalibrationDatum.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Xu;
 using Xu.Chart;
+using System.Reflection;
+using System.Security.Policy;
 
 namespace TestFSQ
 {
@@ -35,8 +37,166 @@ namespace TestFSQ
     }
 
 
+    public class Sprinkle
+    {
 
 
+        public bool SetupEnable
+        {
+            get => SetupControlE.Enabled;
+            set => SetupControlW.Enabled = SetupControlE.Enabled = value;
+        }
+
+        public Control SetupControlE { get; set; }
+        public Control SetupControlW { get; set; }
+
+
+        public Task SprinklerTask { get; set; }
+
+        public void Sprinkler()
+        {
+
+
+        }
+    }
+
+    public interface ISprinkler 
+    {
+        string Address { get; }
+
+        void Start();
+
+        void Stop();
+    }
+
+    public class FancySprinker : ISprinkler 
+    {
+        public void Start() => Send("Start");
+    }
+
+
+    public class OldSchoolSprinker : ISprinkler
+    {
+        public void Start() => Send("Begin");
+    }
+
+    public class Zone 
+    {
+        public Zone(string name) => Name = name;
+
+        public string Name { get; }
+
+        public List<ISprinkler> Sprinklers;
+
+        public void Start() => Sprinklers.ForEach(n=>n.Start);
+
+        public void Stop() => Sprinklers.ForEach(n => n.Stop);
+    }
+
+
+
+    public class RandomTest
+    {
+
+
+
+
+        public int GetRandom(int min, int max)
+        {
+            /*
+            int d = Random().ToInt32();
+
+            d = d % (max - min) + min;
+            return d;
+            */
+
+            return (Random() * (max - min)).ToInt32() + min;
+
+            /*
+            int d = min - 1;// rand();
+
+
+
+            while (d < min && d > max) 
+            {
+        
+                //d = c.ToInt32();
+
+            }
+          */
+        }
+
+        public int[] GetPRBS(int min, int max)
+        {
+            int s = GetRandom(min, max);
+
+            List<int> plist = new List<int>();
+
+            for (int i = 0; i < sizeof(int); i++)
+            {
+                plist.Add((s >> i) & 0x1);
+            }
+
+            return plist.ToArray();
+        }
+
+        public int GetPRBSAlt() => GetRandom(int.MinValue, int.MaxValue);
+
+        public int GetPRBS() => GetRandom(0, 1);
+
+        public int[] GetPRBS(int length)
+        {
+            List<int> plist = new List<int>();
+
+            for (int i = 0; i < length; i++)
+            {
+                plist.Add(GetRandom(0, 1));
+            }
+
+            return plist.ToArray();
+        }
+
+        public void Swap(List<int> source, int X, int Y)
+        {
+            int i_X = -1, i_Y = -1;
+
+            for (int i = 0; i < source.Count; i++)
+            {
+                if (source[i] == X) i_X = i;
+                else if (source[i] == Y) i_Y = i;
+
+                if (i_X > 0 && i_Y > 0)
+                {
+                    source[i_X] = Y;
+                    source[i_Y] = X;
+                    break;
+                }
+            }
+        }
+
+        public void Swap(List<int> XY)
+        {
+            if (XY[0] > XY[1])
+            {
+                int x = XY[0];
+                XY[0] = XY[1];
+                XY[1] = x;
+            }
+        }
+
+        public int IndexOfSorted(int needle, int[] haystack)
+        {
+            //var list = haystack.OrderBy(n => n);
+
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if (haystack[i] == needle) return i;
+            }
+
+            return -1;
+        }
+
+    }
 
 
 
